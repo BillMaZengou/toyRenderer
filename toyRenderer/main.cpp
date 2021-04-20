689ed2a826118b566eda05d209aeecdc52f822b8
Bill Ma <mazengou@gmail.com>
2021-04-20 23:07:54 +0800

    Initial Commit

---
+++ toyRenderer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  toyRenderer
+//
+//  Created by Bill Ma on 2021/4/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
